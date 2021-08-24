8d76712fc861c34a10713ab5760d975d1831eb98
anantterkar <82640594+anantterkar@users.noreply.github.com>
2021-08-24 09:16:10 +0530

    Created using Colaboratory

---
+++ hello_world.py
@@ -0,0 +1,64 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled1.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyMm2LdDuvtrVAIs0MKyiR9A",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/anantterkar/hello-world/blob/main/hello_world.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "JGtP7efyODEL"
+      },
+      "source": [
+        "First python program to print \"Welcome to OOP course\""
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "H_RLfEArOVWY",
+        "outputId": "51e72d1a-4a7c-4add-cb5b-349bf5aadcba"
+      },
+      "source": [
+        "#print is used to output a string of characters or numbers\n",
+        "print(\"Welcome to OOP course\")"
+      ],
+      "execution_count": 3,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "Welcome to OOP course\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    }
+  ]
+}
